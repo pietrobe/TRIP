f8e9af4a749f10687a640da04165b7ccbd32861f
Simone <sriva@susePC>
2024-07-26 15:36:02 +0200

    fix in bash script

--- bin/run_test_64x64.sh
+++ bin/run_test_64x64.sh
@@ -16,7 +16,9 @@
 ### #SBATCH  --error=/home/usi/usi441290/ms5_jobs_log/PRD_3D_64x64_%j.err
 
 #SBATCH --mail-type=all
-#SBATCH --mail-user=simone.riva@usi.ch    //// Per ricevere email di notifica ci si deve registrare in https://userportal.bsc.es/
+#SBATCH --mail-user=simone.riva@usi.ch    
+
+#### Per ricevere email di notifica ci si deve registrare in https://userportal.bsc.es/
 
 srun solar_3D -ksp_monitor -ksp_monitor_true_residual  -ksp_view -ksp_rtol 1e-4 -ksp_max_it 1
  
