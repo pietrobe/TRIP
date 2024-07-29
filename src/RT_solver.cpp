f5471345e4379b54194bc68f30c265f8ddc3bc1c
Simone <sriva@susePC>
2024-07-29 14:55:17 +0200

    more detailed memory usage

--- src/RT_solver.cpp
+++ src/RT_solver.cpp
@@ -2023,6 +2023,8 @@ void MF_context::set_up_emission_module(){
 
     if (mpi_rank_ == 0) std::cout << std::endl << fs_mem_stat.to_string() << std::endl;
 
+    if (mpi_rank_ == 0) std::cout << std::endl << fs_mem_stat.sam_memory_stat.to_string() << std::endl;
+
     if (mpi_rank_ == 0) std::cout << "done" << std::endl;
 
     // std::cout << ecc_sh_ptr_->print_atmos_parameters(0,0,1);
