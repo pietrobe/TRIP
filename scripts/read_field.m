0b4e9ea4212991627bfe651bbc85be9e93ca35c8
Simone <sriva@susePC>
2024-09-04 11:53:33 +0200

    read_field completed

--- scripts/read_field.m
+++ scripts/read_field.m
@@ -11,7 +11,8 @@ if i_theta <= size(Field,2)/2
 end
 
 % load_nu_grid;
-nu_grid = nu_grid(:,1);
+% nu_grid = nu_grid_(:,1);
+nu_grid = vacuum_to_air(freq_to_angstrom(nu_grid_));
 
 mu  = mu_grid(i_theta);
 chi = chi_grid(i_chi);
@@ -22,7 +23,7 @@ xmax = 4228;
 subplot(2,2,1)
 set_plot_defaults
 plot(nu_grid,Field{1,i_theta,i_chi})
-xlim([xmin, xmax])
+% xlim([xmin, xmax])
 hold on
 title(['$(\mu,\chi) = ($', num2str(mu,3), ', ', num2str(chi,3),')'])
 ylabel('$I$')
@@ -51,5 +52,46 @@ xlim([xmin, xmax])
 hold on
 ylabel('$V/I[\%]$')
 set_plot_defaults
+
+function set_plot_defaults()
+ 
+    set(gca,'FontSize', 16);
+    set(gca,'TickLabelInterpreter', 'Latex');    
+    set(groot, 'defaultLegendInterpreter','latex');
+    set(0, 'DefaultTextInterpreter', 'latex')
+    set(0, 'DefaultLineLineWidth', 2);   
+    grid on
+ 
+end
+
+
+ 
+% Function to convert centimeters to angstroms
+function angstrom = cm_to_angstrom(cm)
+    angstrom = cm .* 1e8;
+end
+ 
+ 
+% Function to convert frequency to angstroms
+function angstrom = freq_to_angstrom(freq)
+    speed_of_light = 29979245800; % Speed of light in centmeters per second
+    cm = speed_of_light ./ freq;
+    angstrom = cm_to_angstrom(cm);
+end
+ 
+% Function to convert vacuum wavelength to air wavelength
+function wave_air = vacuum_to_air(wave, to_air_limit)
+    % Default value for to_air_limit if not provided
+    if nargin < 2
+        to_air_limit = 200.0;
+    end
+    
+    wave2 = (wave .* wave) / 100.0;
+    
+    fact = 1.0 + 2.735182e-4 + (1.314182e0 + (1.0 / 2.76249e+4) * wave2) ./ wave2;
+    fact = fact .* (wave > to_air_limit) + 1.0 * (wave < to_air_limit);
+    
+    wave_air = wave ./ fact;
+end
 
 
