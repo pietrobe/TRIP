b5899e3d765e6e4802707e6e8c62f358d7b31cb6
Simone <sriva@susePC>
2024-09-09 15:10:48 +0200

    Update job name in run_ms5.sh script

--- bin/run_ms5.sh
+++ bin/run_ms5.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=01:00:00
 #SBATCH --account=iac90
-#SBATCH --job-name="PRT_3D"
+#SBATCH --job-name="TRIP_PRD_3D"
 #SBATCH --qos=gp_resa
 #SBATCH --nodes=1
 ## #SBATCH --constrain=mc
