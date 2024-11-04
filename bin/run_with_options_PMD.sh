d4d37d6a7c8dc0dc68f1f0e08734b03a9bf15d75
Simone <sriva@susePC>
2024-11-04 09:01:08 +0100

    Update CMakeLists.txt and script to run on MN5

--- bin/run_with_options_PMD.sh
+++ bin/run_with_options_PMD.sh
@@ -1,16 +1,29 @@
 #!/bin/bash -l
 
+### to be defined, 12288 is tested !!!!!!!!!
+### #SBATCH --ntasks=12288
+### #SBATCH --ntasks=16384
 #SBATCH --ntasks=2048
-#SBATCH --cpus-per-task=1
-#SBATCH --constrain=gpu
-#SBATCH --time=0:13:00
-#SBATCH --account=u0
-#SBATCH --job-name="TRIP"
 
-export CRD="--CRD"
+## to be defined !!!!!!!!! 
+#SBATCH --time=02:15:00
+
+#### #SBATCH --cpus-per-task=1
+#SBATCH --account=iac90
+#SBATCH --job-name="TRIP_PRD_3D"
+#SBATCH --qos=gp_resa
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=simone.riva@usi.ch
+
+# export CRD="--CRD"
+export CRD=" "
 
 ### CSCS
-export MAIN_DATA_DIR=/scratch/snx3000/sriva/test_3d/input_new/Comparison-TRIP-PORTA
+
+export MAIN_DATA_DIR=/gpfs/projects/iac90/PORTA
+
+# export MAIN_DATA_DIR=/gpfs/projects/iac90/input/DataSet_TRIP_PORTA/Input_Data_TRIP_PORTA_64x64
 # export SERIES_DIR=Comparison-TRIP-PORTA-20240827T115438Z-001-CRD
 # export GRID_SIZE=64
 
@@ -20,10 +33,11 @@ export MAIN_DATA_DIR=/scratch/snx3000/sriva/test_3d/input_new/Comparison-TRIP-PO
 #export GRID_SIZE=32
 
 export INPUT_DIR=${MAIN_DATA_DIR}/
-export OUTPUT_DIR=${MAIN_DATA_DIR}/output/
+export OUTPUT_DIR=${MAIN_DATA_DIR}/output_test_extra_mu_VIII/
 
 ## For 32 x 32 grid size
-export INPUT_PMD=AR_385_Cut_64x64_mirrorxy-CRD_I_V0_conv_KQ.pmd
+export INPUT_PMD=cai_0Bx_0By_0Bz_1Vx_1Vy_1Vz_GT4_5x5x133_it100.pmd
+
 # export INPUT_CONFIG=AR_385_Cut_32x32-CRD_I_B_V0.conf
 # export INPUT_CONFIG=AR_385_Cut_32x32-CRD_I_B_V.conf
 # export INPUT_CONFIG=AR_385_Cut_32x32-CRD_I_B0_V.conf
@@ -38,7 +52,7 @@ echo ""
 echo "Running the simulation..."
 echo ""
 
-srun ./solar_3D $CRD --input_dir $INPUT_DIR  --problem_pmd_file $INPUT_PMD --output_dir $OUTPUT_DIR -ksp_type fgmres -ksp_gmres_restart 30 -ksp_max_it 200 -ksp_monitor -ksp_view -ksp_rtol 1e-8
+srun /home/usi/usi441290/git/solar_3d/build/solar_3D $CRD --input_dir $INPUT_DIR  --problem_pmd_file $INPUT_PMD --output_dir $OUTPUT_DIR -ksp_type fgmres -ksp_gmres_restart 30 -ksp_max_it 14 -ksp_monitor -ksp_view -ksp_rtol 1e-2
 
 
 
--- mn_bin/Archivio/CRD_run_with_options_B0V.sh
+++ mn_bin/Archivio/CRD_run_with_options_B0V.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -l
 
 ### to be defined, 12288 is tested !!!!!!!!!
-#SBATCH --ntasks=16384
+##### #SBATCH --ntasks=16384
+#SBATCH --ntasks=12288
 
 ## to be defined !!!!!!!!! 
-#SBATCH --time=00:25:00
+#SBATCH --time=10:12:27
 
 #### #SBATCH --cpus-per-task=1
 #SBATCH --account=iac90
@@ -18,11 +19,9 @@
 
 ###### #SBATCH --nodes=1
 
-export CRD="--CRD"
-export MAX_ITER=3
 
-# export CRD=" "
-#  export MAX_ITER=23
+export CRD="--CRD"
+export MAX_ITER=170
 
 ### MareNostrum 5
 export MAIN_DATA_DIR=/gpfs/projects/iac90/
@@ -30,11 +29,11 @@ export MAIN_DATA_DIR=/gpfs/projects/iac90/
 export SERIES_DIR=DataSet_TRIP_PORTA
 
 ### Set the horizontal grid size
-export GRID_SIZE=Input_Data_TRIP_PORTA_64x64
+export GRID_SIZE=64x64
 
 ### MareNostrum5
-export INPUT_DIR=${MAIN_DATA_DIR}/input/${SERIES_DIR}/${GRID_SIZE}
-export OUTPUT_DIR=${MAIN_DATA_DIR}/output_prova/${SERIES_DIR}/${GRID_SIZE}
+export INPUT_DIR=${MAIN_DATA_DIR}/input/${GRID_SIZE}
+export OUTPUT_DIR=${MAIN_DATA_DIR}/output/main/${GRID_SIZE}   ### ATTENTION: test output directory
 
 export RTOL=1e-8
 
@@ -44,30 +43,30 @@ export RTOL=1e-8
 ## 3 TODO
 ## make for thest also with the classic RII grid (fast)
 ## For 64 x 64 grid size in the presence of Magnetic and bulk velocity fields
-export INPUT_CONFIG=AR_385_Cut_64x64_mirrorxy-CRD_I_B_V.conf
+# export INPUT_CONFIG=AR_385_Cut_64x64_mirrorxy-CRD_I_B_V.conf
 
-## 3.1 TODO
+## 3.1 NOT TODO (non serve ormai il FAST e' sufficientemente verificato)
 ## make for thest also with the classic RII grid (fast)
 ## For 64 x 64 grid size in the presence of Magnetic and bulk velocity fields
-# export INPUT_CONFIG=AR_385_Cut_64x64_mirrorxy-CRD_I_B_V.conf
+## export INPUT_CONFIG=AR_385_Cut_64x64_mirrorxy-CRD_I_B_V.conf
 
 #################
 
 ## 1 TODO
 ## For 64 x 64 grid size in the absence of Magnetic and bulk velocity fields
-# export INPUT_CONFIG=AR_385_Cut_64x64_mirrorxy-CRD_I_B0_V0.conf
+## export INPUT_CONFIG=AR_385_Cut_64x64_mirrorxy-CRD_I_B0_V0.conf
 
 #################
 
 ## 2 TODO
 ## For 64 x 64 grid size in the presence of Magnetic and absence of bulk velocity fields
-# export INPUT_CONFIG=AR_385_Cut_64x64_mirrorxy-CRD_I_B_V0.conf
+### export INPUT_CONFIG=AR_385_Cut_64x64_mirrorxy-CRD_I_B_V0.conf
 
 #################
 
 ## 4 TODO
 ## For 64 x 64 grid size in the absence of Magnetic and presence of bulk velocity fields
-# export INPUT_CONFIG=AR_385_Cut_64x64_mirrorxy-CRD_I_B0_V.conf
+## export INPUT_CONFIG=AR_385_Cut_64x64_mirrorxy-CRD_I_B0_V.conf
 ######################################################
 
 echo "INPUT_DIR:     $INPUT_DIR"
