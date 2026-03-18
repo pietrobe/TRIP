8176fc5f47f614f8c77fb2916384261dc26e8cb4
Simone Riva <sriva@daint-ln004.cscs.ch>
2026-03-18 16:36:10 +0100

    fix cmake

--- bin/run_acc_alps.sh
+++ bin/run_acc_alps.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -l
 
 ### to be defined, 12288 is tested !!!!!!!!!
-#### #SBATCH --ntasks=12288
+#SBATCH --ntasks=12288
 ### #SBATCH --ntasks=16384
 #### #SBATCH --ntasks=6144
-#SBATCH --ntasks=512
+##### #SBATCH --ntasks=512
 
 ## to be defined !!!!!!!!! 
-#SBATCH --time=00:30:00
+#SBATCH --time=04:59:59
 
 #SBATCH --job-name="TRIP_PRD_3D"
 
 #SBATCH --exclusive
 #SBATCH --cpus-per-task=1
-#SBATCH --account=u2
-#SBATCH --partition=debug
+#SBATCH --account=c40
+#SBATCH --partition=normal
 #SBATCH --ntasks-per-socket=46
 
 
